42ae71374119360c24b512e255071eff57904eb5
HALASZ\Attila <attilah91@gmail.com>
2018-10-26 15:35:24 +0200

    add libgdx framework

---
+++ desktop/src/hu/halasz/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package hu.halasz.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import hu.halasz.Pannonia;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Pannonia(), config);
+	}
+}
