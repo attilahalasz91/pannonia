7c32174059b412e80957741609340b581aed3ad3
HALASZ\Attila <attilah91@gmail.com>
2018-10-26 15:32:19 +0200

    initial commit

---
+++ android/src/hu/halasz/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package hu.halasz;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import hu.halasz.Pannonia;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Pannonia(), config);
+	}
+}
